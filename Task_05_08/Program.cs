b6acf29b3b952b5ad47872831fc82fe5d99ea2d5
Сергей ⠀ <yachiter001@gmail.com>
2025-01-31 08:11:38 +0500

    Завершены все задания 5 урока

---
+++ Task_05_08/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Task_05_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Даны два массива размером 10*10, заполненные целыми числами в диапазоне от 1 до 9 вкл. Создать новый массив, который будет
+             * произведением двух предыдущих (перемножить элементы первых двух массивов, стоящие на одинаковых позициях и записать их в
+             * результирующий массив)
+             * Вывести результирующий массив 
+             */
+
+            int[,] matrix1 = new int[10, 10];
+            int[,] matrix2 = new int[10, 10];
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)        //Заполнение массивов
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    matrix1[i, j] = random.Next(1, 10);
+                }
+                for (int k = 0; k < 10; k++)
+                {
+                    matrix2[i, k] = random.Next(1, 10);
+                }
+            }
+
+            Console.WriteLine("Первые две матрицы: ");
+            Console.WriteLine();
+            
+            for (int i = 0;i < 10; i++)         //Вывод первых двух матриц
+            {
+                for (int j = 0;j < 10; j++)
+                {
+                    Console.Write(matrix1[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(matrix2[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Произведение матриц: ");
+            Console.WriteLine();
+
+            int[,] multipliedMatrix = new int[10, 10];
+            
+            for (int i = 0; i < 10; i++)            //Перемножение матриц
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    multipliedMatrix[i, j] = matrix1[i, j] * matrix2[i, j];
+                }
+            }
+
+            for (int i = 0; i < 10; i++)            //Вывод умножения матриц
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(multipliedMatrix[i, j] + "   ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task_05_09/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Task_05_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Дан квадратный массив размерность n*n. Произведите анализ данной матрицы и выясните является ли данная матрица Z-матрицей
+             * (это матрица, где все недиагональные элементы меньше нуля)
+             * Если данное условие выполняется то вывести данную матрицу с цветовой индикацией главной диагонали. Если не выполняется, то
+             * вывести сообщение что данная матрица не является Z-матрицей 
+             */
+
+            Console.WriteLine("Введите размер матрицы: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];      
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)     //Заполнение массива
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = random.Next(-10, 11);
+                }
+            }
+
+            bool isZMatrix = true;      //Проверка на Z-матрицу
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j && matrix[i, j] >= 0)
+                    {
+                        isZMatrix = false;
+                        break;
+                    }
+                }
+                if (!isZMatrix)
+                    break;
+            }
+
+            if (isZMatrix)      //Вывод Z-матрицы
+            {
+                Console.WriteLine("Матрица является Z-матрицей: ");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        Console.Write(matrix[i, j] + " ");
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine();
+                }
+            }       
+            else                //Если не является Z-матрицей
+            {
+                Console.WriteLine("Матрица не является Z-матрицей.");
+            }
+
+        }
+    }
+}
